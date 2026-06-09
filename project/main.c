fb685b97125471a2d618fae2c437f59815b941be
Gustavo Cleofas da Silva <gustavo.cleofas2009@gmail.com>
2026-06-09 13:24:00 -0300

    Update main.c

--- project/main.c
+++ project/main.c
@@ -1,71 +1,115 @@
-#include <stdio.h>
+#include <stdio.h> // Biblioteca padrão para usar printf()
+// Biblioteca principal do FreeRTOS
 #include "freertos/FreeRTOS.h"
+// Biblioteca para usar tasks e delays
 #include "freertos/task.h"
+// Biblioteca para controlar os GPIOs da ESP32
 #include "driver/gpio.h"
-// Controle PWM do servo
+// Biblioteca para controle PWM do servo motor
 #include "driver/ledc.h"
-// Delay em microssegundos
+// Biblioteca para delays em microssegundos
 #include "esp_rom_sys.h"
-// Timer da ESP32
+// Biblioteca para medir tempo em microssegundos
 #include "esp_timer.h"
+
+
 // DEFINIÇÃO DOS PINOS
 
-
-// Pino TRIG do HC-SR04 - Ele vai enviar o ultrassom.
+// Pino TRIG do HC-SR04
+// Responsável por ENVIAR o ultrassom
 #define TRIG_PIN GPIO_NUM_12
 
-// Pino ECHO do HC-SR04 - Ele vai receber o retorno do ultrassom.
+// Pino ECHO do HC-SR04
+// Responsável por RECEBER o retorno do ultrassom
 #define ECHO_PIN GPIO_NUM_13
 
-// Pino do servo motor - Ele vai controlar o servo motor.
+// Pino conectado ao servo motor
+// Responsável por controlar a abertura da porta
 #define SERVO_PIN GPIO_NUM_14
 
-// FUNÇÃO PARA MEDIR DISTÂNCIA e retorna valor decimal.
+// FUNÇÃO PARA MEDIR DISTÂNCIA
 
+// Função do tipo float
+// Ela retorna um valor decimal da distância
 float medir_distancia()
 {
-    // Variáveis do tempo 
-    // vai criar um tempo inicial e um tempo final.
+   
+    // VARIÁVEIS DE TEMPO
+    
+    // Variável do tempo inicial
     int64_t inicio = 0;
+
+    // Variável do tempo final
     int64_t fim = 0;
 
-    // Garante TRIG desligado
+   
+    // DISPARO DO HC-SR04
+    
+
+    // Garante que o TRIG comece desligado
     gpio_set_level(TRIG_PIN, 0);
 
     // Espera 2 microssegundos
     esp_rom_delay_us(2);
 
-    // Liga TRIG e envia ultrassom.
+    // Liga o TRIG
+    // Isso faz o sensor enviar o ultrassom
     gpio_set_level(TRIG_PIN, 1);
 
-    // Mantém ligado por 10 microssegundos (Esse é o tempo exigido pelo HC-SR04).
+    // Mantém ligado por 10 microssegundos
+    // Esse é o tempo exigido pelo HC-SR04
     esp_rom_delay_us(10);
 
-    // Desliga TRIG - O pulso terminou.
+    // Desliga o TRIG
+    // O pulso terminou
     gpio_set_level(TRIG_PIN, 0);
 
     
+    // ESPERA O ECHO COMEÇAR
+    
+    /*
+       Enquanto o ECHO estiver LOW:
+       significa que o ultrassom ainda não voltou.
+    */
+
     while (gpio_get_level(ECHO_PIN) == 0)
-    /* ESP espera:  ECHO ficar HIGH, Enquanto estiver LOW:o som 
-    ainda não voltou */
     {
     }
 
-    // o instante exato que o ECHO começou.
+    // TEMPO INICIAL
+    
+    // Guarda o instante exato que o ECHO começou
     inicio = esp_timer_get_time();
 
-    //Enquanto ECHO estiver HIGH: o som ainda está retornando.
+    // ESPERA O ECHO TERMINAR
+     /*
+       Enquanto o ECHO estiver HIGH:
+       significa que o ultrassom ainda está retornando.
+    */
+
     while (gpio_get_level(ECHO_PIN) == 1)
     {
     }
-
-    // O instante que o retorno terminou.
+// TEMPO FINAL -------
+    
+    // Guarda o instante que o retorno terminou
     fim = esp_timer_get_time();
 
-/* Vai calcular o: tempo final - tempo inicial. Vai sair
-o Resultado: tempo do ultrassom*/
+    
+    // CÁLCULO DO TEMPO
+    
+
+    /*
+       Calcula:
+tempo final - tempo inicial
+Resultado:
+  tempo total do ultrassom
+    */
+
     float tempo = fim - inicio;
 
+// CÁLCULO DA DISTÂNCIA
+    
 /* Vamos criar uma variável  float para calcular a distância
 detectada pelo sensor ultrassônico. Usamos float porque o valor
 da distância pode possuir casas decimais.
@@ -74,104 +118,128 @@ A divisão é por 2 porque o ultrassom percorre o caminho de ida
 até o objeto e depois retorna ao sensor. Ai dividimos por 2 por que
 prescisamos do resultado para obter apenas a distância entre o
 sensor e o objeto.*/
+
+
     float distancia = tempo * 0.034 / 2;
 
-    // Retorna a distância medida.
+    // Retorna a distância medida
     return distancia;
 }
 void app_main()
-{  // CONFIGURA GPIOs
-
-    // TRIG como saída, envia o sinal
-    gpio_set_direction(TRIG_PIN, GPIO_MODE_OUTPUT);
-
-    // ECHO como entrada, recebe o sinal
-    gpio_set_direction(ECHO_PIN, GPIO_MODE_INPUT);
-
-  // CONFIGURA PWM DO SERVO, a parte mais dificil por não conhecer nada.
+{
     
-      ledc_timer_config_t timer = { //Cria estrutura do timer PWM
-        .speed_mode = LEDC_HIGH_SPEED_MODE, //modo rápido
-        .timer_num = LEDC_TIMER_0, //timer 0
-        .duty_resolution = LEDC_TIMER_13_BIT, /*Define resolução do PWM 13
-        bits: maior precisão. */
-        .freq_hz = 50, // Mesma Frequência do servo motor
-        .clk_cfg = LEDC_AUTO_CLK}; //ESP32 escolhe automaticamente o clock do PWM
+    // CONFIGURAÇÃO DOS GPIOs
+    
 
-    // Aplica configuração do timer
-    ledc_timer_config(&timer);
+    // Configura o TRIG como saída, porque ele envia o sinal
+gpio_set_direction(TRIG_PIN, GPIO_MODE_OUTPUT);
 
-    // Configuração do canal PWM
-    ledc_channel_config_t canal = {
-        .gpio_num = SERVO_PIN,
-        .speed_mode = LEDC_HIGH_SPEED_MODE,
-        .channel = LEDC_CHANNEL_0,
-        .timer_sel = LEDC_TIMER_0,
-        .duty = 0,
-        .hpoint = 0};
+// Configura o ECHO como entrada, porque ele recebe o sinal
+gpio_set_direction(ECHO_PIN, GPIO_MODE_INPUT);
 
-    // Aplica configuração do canal
-    ledc_channel_config(&canal);
 
-    // ==========================
+    // CONFIGURAÇÃO PWM DO SERVO
+   /*
+       Essa parte configura o PWM
+       responsável pelo controle
+       do servo motor.
+  */
+// Cria a estrutura do timer PWM
+ledc_timer_config_t timer = {
+// Modo rápido
+.speed_mode = LEDC_HIGH_SPEED_MODE,
+// Utiliza o timer 0
+.timer_num = LEDC_TIMER_0,
+
+ /*
+Define resolução do PWM.
+13 bits:
+maior precisão no controle.
+*/
+.duty_resolution = LEDC_TIMER_13_BIT,
+// Frequência padrão de servo motor
+ .freq_hz = 50,
+
+/*
+ A ESP32 escolhe automaticamente
+ o clock do PWM.
+*/
+.clk_cfg = LEDC_AUTO_CLK};
+// Aplica a configuração do timer
+ledc_timer_config(&timer);
+
+    // CONFIGURAÇÃO DO CANAL PWM
+    
+ledc_channel_config_t canal = {
+// GPIO do servo motor
+.gpio_num = SERVO_PIN,
+// Modo rápido
+.speed_mode = LEDC_HIGH_SPEED_MODE,
+// Canal PWM 0
+.channel = LEDC_CHANNEL_0,
+// Usa timer 0
+.timer_sel = LEDC_TIMER_0,
+// PWM começa desligado
+.duty = 0,
+// Ponto inicial do PWM
+.hpoint = 0};
+
+// Aplica configuração do canal
+ledc_channel_config(&canal);
+
     // LOOP PRINCIPAL
-    // ==========================
-
+    
     while (1)
     {
-        // Mede distância
-        float distancia = medir_distancia();
+// Mede a distância
+float distancia = medir_distancia();
 
-        // Mostra no terminal
-        printf("Distancia: %.2f cm\n", distancia);
+// Mostra a distância no terminal
+printf("Distancia: %.2f cm\n", distancia);
 
-        // Verifica aproximação
-        if (distancia < 20)
+         // VERIFICA APROXIMAÇÃO
+// Se a distância for menor que 20 cm
+if (distancia < 20)
         {
-            // Mensagem
-            printf("Objeto detectado\n");
+// Mostra mensagem
+printf("Objeto detectado\n");
 
-            // ==========================
-            // ABRIR PORTA
-            // ==========================
+ // ABRIR PORTA
+/*
+Define PWM do servo
+para posição de abertura.*/
+ledc_set_duty(
+LEDC_HIGH_SPEED_MODE,
+LEDC_CHANNEL_0,
+600);
 
-            // PWM para abrir
-            ledc_set_duty(
-                LEDC_HIGH_SPEED_MODE,
-                LEDC_CHANNEL_0,
-                600);
+// Atualiza o PWM
+ledc_update_duty(
+LEDC_HIGH_SPEED_MODE,
+LEDC_CHANNEL_0);
 
-            // Atualiza PWM
-            ledc_update_duty(
-                LEDC_HIGH_SPEED_MODE,
-                LEDC_CHANNEL_0);
+// Mostra mensagem
+printf("Porta aberta\n");
 
-            // Mostra mensagem
-            printf("Porta aberta\n");
+// Espera 3 segundos
+vTaskDelay(pdMS_TO_TICKS(3000));
 
-            // Espera 3 segundos
-            vTaskDelay(pdMS_TO_TICKS(3000));
-
-            // ==========================
-            // FECHAR PORTA
-            // ==========================
-
-            // PWM para fechar
-            ledc_set_duty(
-                LEDC_HIGH_SPEED_MODE,
-                LEDC_CHANNEL_0,
-                200);
-
-            // Atualiza PWM
-            ledc_update_duty(
-                LEDC_HIGH_SPEED_MODE,
-                LEDC_CHANNEL_0);
-
-            // Mostra mensagem
-            printf("Porta fechada\n");
+           
+// FECHAR PORTA
+ /*  Define PWM do servo
+     para posição de fechamento.*/
+ ledc_set_duty(
+ LEDC_HIGH_SPEED_MODE,
+LEDC_CHANNEL_0,
+200);
+// Atualiza PWM
+ ledc_update_duty(
+ LEDC_HIGH_SPEED_MODE,
+ LEDC_CHANNEL_0);
+ // Mostra mensagem
+ printf("Porta fechada\n");
         }
-
-        // Espera 500 ms
-        vTaskDelay(pdMS_TO_TICKS(500));
+// Espera 500 ms antes da próxima leitura
+ vTaskDelay(pdMS_TO_TICKS(500));
     }
 }
